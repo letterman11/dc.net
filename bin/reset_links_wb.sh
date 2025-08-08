1e92e5c83567d971a40911de91b6694b3173e366
Angus Brooks <angusbrooks@gmail.com>
2025-08-08 17:33:42 -0400

    edits

--- bin/reset_links_wb.sh
+++ bin/reset_links_wb.sh
@@ -3,6 +3,6 @@ new_path=
 old_path_2=
 new_path_2=
 
-find  -iname "*.py" -or -iname "*.js"  -or -iname "*.html" -type f | xargs sed -i 's#pyWebMarks#exWebMarks#g'
-find  -iname "*.html"  -type f | xargs sed -i 's#css/#css_ex/#g'
-find  -iname "*.html"  -type f | xargs sed -i 's#js/#js_ex/#g'
+find ~/pyUp/py3x/marksPWAPage -iname "*.py" -or -iname "*.js"  -or -iname "*.html" -type f | xargs sed -i 's#pyWebMarks#ex2WebMarks#g'
+find ~/pyUp/py3x/marksPWAPage  -iname "*.html"  -type f | xargs sed -i 's#/css/#/ex2/css/#g'
+find ~/pyUp/py3x/marksPWAPage  -iname "*.html"  -type f | xargs sed -i 's#/js/#/ex2/js/#g'
